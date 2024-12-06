2877367a0a32cd6a81929dbbea15f563f3d0eb4a
filip_csibi <csibifilip777@gmail.com>
2024-12-06 19:43:27 +0200

    structure

---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TabNavigator = () => {
+  return <div>TabNavigator</div>;
+};
---
+++ src/user/UserContext.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function UserContext() {
+  return (
+    <View>
+      <Text>UserContext</Text>
+    </View>
+  );
+}
